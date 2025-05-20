34dc3badc7f2b2ae757010cd28281fe186ca594a
Akshay B <77104366+akshayb72@users.noreply.github.com>
2025-05-21 02:30:25 +0530

    Add files via upload

---
+++ morse.py
@@ -0,0 +1,52 @@
+def main():
+    while True :
+        option = int(input("""1. Encode a String to Morse code\n2. Decode Morse to a String\n3. Exit\n"""))
+        if option == 1:
+            alph = input("Enter a String: \n").lower()
+            alph_to_morse = from_alphanum_to_morse(alph)
+            print(alph_to_morse)
+        elif option == 2:
+            morse = input('Enter morse code: \n')
+            morse_to_alph = from_morse_to_alphanum(morse)
+            print(morse_to_alph)
+        elif option == 3:
+            break
+        else:
+            print(f"{option} is an invaild option")
+   
+alphanum_to_morse = {
+'1':'.----','2':'..---','3':'...--','4':'....-',
+'5':'.....','6':'-....','7':'--...',
+'8':'---..','9':'----.','0':'-----',
+" ":" ",
+'a':'.-','b':'-...','c':'-.-.','d':'-..','e':'.',
+'f':'..-.','g':'--.','h':'....','i':'..',
+'j':'.---','k':'-.-','l':'.-..','m':'--','n':'-.',
+'o':'---','p':'.--.','q':'--.-','r':'.-.','s':'...',
+'t':'-','u':'..-','v':'...-','w':'.--',
+'x':'-..-','y':'-.--','z':'--..'
+}
+
+def from_alphanum_to_morse(strings):
+    morse_string = ''
+    for string in strings:
+        morse_string += alphanum_to_morse[string]
+        morse_string += ' '
+    return morse_string
+
+def from_morse_to_alphanum(morse_str):    
+    decoded_str = ''
+    morse_string = ''
+    morse_str += ' '
+
+    for string in morse_str:
+        if string != ' ':
+            morse_string += string
+        else:
+            for alphanum, morse in alphanum_to_morse.items():                
+                if (morse_string == morse):
+                    decoded_str += alphanum
+            morse_string = ''
+    return decoded_str
+
+main()
